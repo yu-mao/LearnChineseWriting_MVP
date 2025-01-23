f67ad455b65b62c903b0fa55da60914dc0bb1f81
Jiayao Yu <yjybxx2@yahoo.com>
2025-01-23 23:13:47 +0100

    Added custom index finger writing hand pose detection. Tested on headset.

--- Assets/Scripts/GameSceneController.cs
+++ Assets/Scripts/GameSceneController.cs
@@ -2,15 +2,9 @@ using UnityEngine;
 
 public class GameSceneController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public void WriteAStroke() {}
+    
+    public void ConfirmWriting() {}
+    
+    public void RedoWriting() {}
 }
