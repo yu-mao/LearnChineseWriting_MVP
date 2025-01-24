de216b8f9ea8527b33b40483294d8d9f55fde316
Jiayao Yu <yjybxx2@yahoo.com>
2025-01-24 15:04:23 +0100

    Improved index finger writing pose recognition. Tested on headset.

--- Assets/Scripts/GameSceneController.cs
+++ Assets/Scripts/GameSceneController.cs
@@ -1,23 +1,37 @@
-using System;
+using System.Collections.Generic;
+using NUnit.Framework;
 using TMPro;
 using UnityEngine;
 
-public enum CustomBoneId
-{
-    // XRHand_IndexTip = OVRSkeleton.BoneId.XRHand_IndexTip,
-    customIndexTip = OVRPlugin.BoneId.XRHand_IndexTip,
-}
-
 public class GameSceneController : MonoBehaviour
 {
+    [Header("Temporary")]
     [SerializeField] private GameObject _tempIndexTipVis;
     [SerializeField] private TextMeshProUGUI _debugText;
 
+    [Header("Hand Setup")]
     [SerializeField] private OVRHand _hand;
     [SerializeField] private OVRSkeleton _handSkeleton;
-    private OVRBone _bone;
-    
-    public void WriteAStroke() {}
+
+    [Header("Visuals")]
+    [SerializeField] private LineRenderer _strokeVisual;
+
+    private bool _isWriting = false;
+    private List<List<Vector3>> _userWrittenStrokes = new List<List<Vector3>>();
+    private List<Vector3> _currentUserWrittenStroke = new List<Vector3>();
+
+    public void StartWriting()
+    {
+        _isWriting = true;
+        _tempIndexTipVis.SetActive(true);
+    }
+
+    public void EndWriting()
+    {
+        _isWriting = false;
+        _currentUserWrittenStroke.Clear();
+        _tempIndexTipVis.SetActive(false);
+    }
     
     public void ConfirmWriting() {}
     
@@ -25,15 +39,25 @@ public class GameSceneController : MonoBehaviour
 
     private void Update()
     {
-        if (_hand.IsTracked)
+        _debugText.text = "isWriting: " + _isWriting + " | strokeCount: " + _currentUserWrittenStroke.Count;
+        if (_isWriting && _hand.IsTracked && _handSkeleton.IsInitialized)
         {
-            DisplayIndexFingerTip();
+            _currentUserWrittenStroke.Add(GetIndexFingerTipPosition());
+            VisualizeStroke(_currentUserWrittenStroke);
         }
     }
 
-    private void DisplayIndexFingerTip()
+    private Vector3 GetIndexFingerTipPosition()
     {
         var indexTip = _handSkeleton.Bones[(int)OVRSkeleton.BoneId.XRHand_IndexTip];
         _tempIndexTipVis.transform.position = indexTip.Transform.position;
+        return indexTip.Transform.position;
+    }
+    
+    private void VisualizeStroke(List<Vector3> currentUserWrittenStroke)
+    {
+        _strokeVisual.positionCount = 0;
+        _strokeVisual.SetPositions(currentUserWrittenStroke.ToArray());
+        _strokeVisual.positionCount = currentUserWrittenStroke.Count;
     }
 }
