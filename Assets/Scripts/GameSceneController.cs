60e7d7028199f6fedf4f665b8fc57d1a1cf6a27c
Jiayao Yu <yjybxx2@yahoo.com>
2025-01-24 12:35:41 +0100

    Added index finger tip position tracking. Tested on headset.

--- Assets/Scripts/GameSceneController.cs
+++ Assets/Scripts/GameSceneController.cs
@@ -1,12 +1,39 @@
+using System;
+using TMPro;
 using UnityEngine;
+
+public enum CustomBoneId
+{
+    // XRHand_IndexTip = OVRSkeleton.BoneId.XRHand_IndexTip,
+    customIndexTip = OVRPlugin.BoneId.XRHand_IndexTip,
+}
 
 public class GameSceneController : MonoBehaviour
 {
     [SerializeField] private GameObject _tempIndexTipVis;
+    [SerializeField] private TextMeshProUGUI _debugText;
+
+    [SerializeField] private OVRHand _hand;
+    [SerializeField] private OVRSkeleton _handSkeleton;
+    private OVRBone _bone;
     
     public void WriteAStroke() {}
     
     public void ConfirmWriting() {}
     
     public void RedoWriting() {}
+
+    private void Update()
+    {
+        if (_hand.IsTracked)
+        {
+            DisplayIndexFingerTip();
+        }
+    }
+
+    private void DisplayIndexFingerTip()
+    {
+        var indexTip = _handSkeleton.Bones[(int)OVRSkeleton.BoneId.XRHand_IndexTip];
+        _tempIndexTipVis.transform.position = indexTip.Transform.position;
+    }
 }
