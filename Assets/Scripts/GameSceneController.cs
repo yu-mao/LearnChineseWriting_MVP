53eaaf52316be60f8667fbb40a5821f54c165a7c
Jiayao Yu <yjybxx2@yahoo.com>
2025-01-27 22:38:23 +0100

    Added multi-stroke writing. Tested on headset. To debug initial strokes not shown issue.

--- Assets/Scripts/GameSceneController.cs
+++ Assets/Scripts/GameSceneController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using NUnit.Framework;
 using TMPro;
@@ -5,64 +6,99 @@ using UnityEngine;
 
 public class GameSceneController : MonoBehaviour
 {
-    [Header("Temporary")]
-    [SerializeField] private GameObject _tempIndexTipVis;
+    // [Header("Temporary")]
+    // [SerializeField] private GameObject _tempIndexTipVis;
     [SerializeField] private TextMeshProUGUI _debugText;
 
     [Header("Hand Setup")]
     [SerializeField] private OVRHand _hand;
     [SerializeField] private OVRSkeleton _handSkeleton;
 
-    [Header("Visuals")]
-    [SerializeField] private LineRenderer _strokeVisual;
+    [Header("Visual Feedback")]
+    [SerializeField] private UserWritingVisualFeedback _userWritingVisualFeedback;
 
     private bool _isWriting = false;
+    private bool _wasWriting = false;
     private List<List<Vector3>> _userWrittenStrokes = new List<List<Vector3>>();
     private List<Vector3> _currentUserWrittenStroke = new List<Vector3>();
+    private int _maxCountUserWrittenStrokes;
+    private int _countUserWrittenStrokes;
 
     public void StartWriting()
     {
         _isWriting = true;
-        _tempIndexTipVis.SetActive(true);
+        // _tempIndexTipVis.SetActive(true);
     }
 
     public void EndWriting()
     {
         _isWriting = false;
         _currentUserWrittenStroke.Clear();
-        _tempIndexTipVis.SetActive(false);
+        // _tempIndexTipVis.SetActive(false);
     }
 
     public void ConfirmWriting()
     {
-        _debugText.text = "Confirm";
+        // TODO: randomize writing colour
     }
 
     public void RedoWriting()
     {
-        _debugText.text = "Redo";
+        
+    }
+
+    private void Start()
+    {
+        _maxCountUserWrittenStrokes = _userWritingVisualFeedback.strokes.Count;
     }
 
     private void Update()
     {
-        _debugText.text = "Is Writing: " + _isWriting;
-        if (_isWriting && _hand.IsTracked && _handSkeleton.IsInitialized)
+        if (_hand.IsTracked && _handSkeleton.IsInitialized)
         {
-            _currentUserWrittenStroke.Add(GetIndexFingerTipPosition());
-            VisualizeStroke(_currentUserWrittenStroke.ToArray());
+            if (_isWriting)
+            {
+                _currentUserWrittenStroke.Add(GetIndexFingerTipPosition());
+                UpdateUserWrittenStrokes(_wasWriting);
+                VisualizeCurrentUserWrittenStroke(_currentUserWrittenStroke.ToArray());
+            }
         }
+        _wasWriting = _isWriting;
     }
 
     private Vector3 GetIndexFingerTipPosition()
     {
         var indexTip = _handSkeleton.Bones[(int)OVRSkeleton.BoneId.XRHand_IndexTip];
-        _tempIndexTipVis.transform.position = indexTip.Transform.position;
+        // _tempIndexTipVis.transform.position = indexTip.Transform.position;
         return indexTip.Transform.position;
     }
-
-    private void VisualizeStroke(Vector3[] currentUserWrittenStroke)
+    
+    private void UpdateUserWrittenStrokes(bool wasWriting)
     {
-        _strokeVisual.positionCount = currentUserWrittenStroke.Length;
-        _strokeVisual.SetPositions(currentUserWrittenStroke);
+        if (wasWriting && _userWrittenStrokes.Count >= 1)
+        {
+            // if it's a continuous writing stoke 
+            _userWrittenStrokes[_countUserWrittenStrokes] = _currentUserWrittenStroke;
+        }
+        else
+        {
+            // if it's a new writing stroke
+            _userWrittenStrokes.Add(_currentUserWrittenStroke);
+            if (_countUserWrittenStrokes < _maxCountUserWrittenStrokes)
+            {
+                _countUserWrittenStrokes += 1;
+            }
+            else
+            {
+                _countUserWrittenStrokes = 0;
+            }
+        }
+        _debugText.text = "_countUserWrittenStrokes: " + _countUserWrittenStrokes;
+    }
+    
+    private void VisualizeCurrentUserWrittenStroke(Vector3[] currentUserWrittenStroke)
+    {
+        _userWritingVisualFeedback.strokes[_countUserWrittenStrokes].positionCount = currentUserWrittenStroke.Length;
+        _userWritingVisualFeedback.strokes[_countUserWrittenStrokes].SetPositions(currentUserWrittenStroke);
     }
 }
